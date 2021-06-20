502f1d434f1c3593f1539aec0e6e6302ffd1a3a8
Gudditi <gudditinaganjaneyulu@yahoo.com>
2021-06-20 16:52:36 +0530

    ec2 terraform tmeplete added

---
+++ teraform templetets/ec2instance.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA6LTDUBEDGCRRV6HT"
+  secret_key = "dGNgLppZ+LTaOqFjuUQ7oVxWdRN81+2Mna0YlBbN"
+}
+
+resource "aws_instance" "example" {
+ami = "ami-0c09927662c939f41"
+instance_type = "t2.micro"
+
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpcmain"
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igwmain"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.subnet2.id
+  tags = {
+    Name = "ngw"
+  }
+}
+
+resource "aws_eip" "lp" {
+  vpc      = true
+}
+
+resource "aws_route_table" "rtt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = {
+    Name = "routetable"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtt.id
+}
+
+resource "aws_route_table_association" "b" {
+  gateway_id     = aws_internet_gateway.igw.id
+  route_table_id = aws_route_table.rtt.id
+}
